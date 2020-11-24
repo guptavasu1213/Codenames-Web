458258fbcb1890d523a8af6129b57abc42f1ffe2
Ahsan Hassan <ahsanhassan@Ahsans-MacBook-Pro.local>
2020-11-24 03:03:39 -0700

    gameplay template started

---
+++ dist/javascript/gameplay.js
@@ -0,0 +1,91 @@
+// function btnClick() {
+//     var x = document.getElementById("mytable").getElementsByTagName("td");
+//     x[0].innerHTML = "i want to change my cell color";
+//     x[0].style.backgroundColor = "yellow";            
+// }
+
+
+var 
+gameBoard = document.querySelector("#gameboard").querySelectorAll("td")
+box1 = gameBoard[0], box2 = gameBoard[1], box3 = gameBoard[2], box4 = gameBoard[3], box5 = gameBoard[4], 
+box6 = gameBoard[5], box7 = gameBoard[6], box8 = gameBoard[7], box9 = gameBoard[8], box10 = gameBoard[9],
+box11 = gameBoard[10], box12 = gameBoard[11], box13 = gameBoard[12], box14 = gameBoard[13], box15 = gameBoard[14],
+box16 = gameBoard[15], box17 = gameBoard[16], box18 = gameBoard[17], box19 = gameBoard[18],box20 = gameBoard[19],
+box21 = gameBoard[20], box22 = gameBoard[21], box23 = gameBoard[22], box24 = gameBoard[23],box25 = gameBoard[24]
+
+box1.addEventListener("click", onBox1Click);
+box2.addEventListener("click", onBox2Click);
+box3.addEventListener("click", onBox3Click);
+box4.addEventListener("click", onBox4Click);
+box5.addEventListener("click", onBox5Click);
+box6.addEventListener("click", onBox6Click);
+box7.addEventListener("click", onBox7Click);
+box8.addEventListener("click", onBox8Click);
+box9.addEventListener("click", onBox9Click);
+box10.addEventListener("click", onBox10Click);
+box11.addEventListener("click", onBox11Click);
+box12.addEventListener("click", onBox12Click);
+box13.addEventListener("click", onBox13Click);
+box14.addEventListener("click", onBox14Click);
+box15.addEventListener("click", onBox15Click);
+box16.addEventListener("click", onBox16Click);
+box17.addEventListener("click", onBox17Click);
+box18.addEventListener("click", onBox18Click);
+box19.addEventListener("click", onBox19Click);
+box20.addEventListener("click", onBox20Click);
+box21.addEventListener("click", onBox21Click);
+box22.addEventListener("click", onBox22Click);
+box23.addEventListener("click", onBox23Click);
+box24.addEventListener("click", onBox24Click);
+box25.addEventListener("click", onBox25Click);
+
+function onBox1Click(){
+}
+function onBox2Click(){
+}
+function onBox3Click(){
+}
+function onBox4Click(){
+}
+function onBox5Click(){
+}
+function onBox6Click(){
+}
+function onBox7Click(){
+}
+function onBox8Click(){
+}
+function onBox9Click(){
+}
+function onBox10Click(){
+}
+function onBox11Click(){
+}
+function onBox12Click(){
+}
+function onBox13Click(){
+}
+function onBox14Click(){
+}
+function onBox15Click(){
+}
+function onBox16Click(){
+}
+function onBox17Click(){
+}
+function onBox18Click(){
+}
+function onBox19Click(){
+}
+function onBox20Click(){
+}
+function onBox21Click(){
+}
+function onBox22Click(){
+}
+function onBox23Click(){
+}
+function onBox24Click(){
+}
+function onBox25Click(){
+}
