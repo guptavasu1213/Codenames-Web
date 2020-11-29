1b018bb6404826e54899f4dd87cfdc7d6639774a
hixxixxi <55465923+hixxixxi@users.noreply.github.com>
2020-11-28 17:28:58 -0700

    Fix create new game and struct name

--- common.go
+++ common.go
@@ -11,9 +11,9 @@ import (
 )
 
 type links struct {
-	RedLink  string `json:"redLink"`
-	BlueLink string `json:"blueLink"`
-	SpyLink  string `json:"spyLink"`
+	RedCode  string `json:"redCode"`
+	BlueCode string `json:"blueCode"`
+	SpyCode  string `json:"spyCode"`
 }
 
 type card struct {
